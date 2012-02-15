3751901d63c5bc0dd04904d5d142b54be33d8f1a
Pepe Doval <pepellou@gmail.com>
2012-02-15 20:19:48 +0100

    Added value 2 to primes vector

--- src/katas/refactor/euler/ProblemSolver10.java
+++ src/katas/refactor/euler/ProblemSolver10.java
@@ -6,12 +6,14 @@ public class ProblemSolver10 extends ProblemSolver {
 
 	@Override
 	public String solve() {
-		return "" + (sumValues(getPrimesUpTo(2000000), Condition.truth()) + 2);
+		return "" + sumValues(getPrimesUpTo(2000000), Condition.truth());
 	}
 
 	private Long[] getPrimesUpTo(int topValue) {
 		Vector<Long> primes = new Vector<Long>();
 		Vector<Long> squares_of_primes = new Vector<Long>();
+		primes.add(2l);
+		squares_of_primes.add(4l);
 		for (long natural = 3; natural < topValue; natural += 2) {
 			boolean is_prime = true;
 			for (int pr = 0; pr < primes.size() && is_prime
