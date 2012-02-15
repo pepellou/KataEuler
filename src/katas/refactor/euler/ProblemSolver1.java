6f8476ef609ea4b1368600a9339229c66df8ab8d
Pepe Doval <pepellou@gmail.com>
2012-02-15 11:55:36 +0100

    Increase legibility

--- src/katas/refactor/euler/ProblemSolver1.java
+++ src/katas/refactor/euler/ProblemSolver1.java
@@ -4,13 +4,15 @@ public class ProblemSolver1 extends ProblemSolver {
 
 	@Override
 	public String solve() {
-		int sum = 0;
 		int naturals[] = new int[1000];
 		for (int natural = 0; natural < 1000; natural++) {
 			naturals[natural] = natural;
 		}
+		int sum = 0;
 		for (int natural : naturals) {
-			if (multipleOf(natural, 3) || multipleOf(natural, 5))
+			boolean condition = multipleOf(natural, 3)
+					|| multipleOf(natural, 5);
+			if (condition)
 				sum += natural;
 		}
 		return "" + sum;
