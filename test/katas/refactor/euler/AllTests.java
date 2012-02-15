e5b27a1d532acd7f24e32ab1b907a52a7f7f925f
Pepe Doval <pepellou@gmail.com>
2012-02-15 11:50:41 +0100

    Test suite for running all tests.

---
+++ test/katas/refactor/euler/AllTests.java
@@ -0,0 +1,11 @@
+package katas.refactor.euler;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ProblemSolver1Test.class, ProblemSolver2Test.class,
+		ProblemSolver10Test.class, EulerTest.class })
+public class AllTests {
+}
