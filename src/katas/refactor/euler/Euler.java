af556ff3209bd847b4496773f35c5cad4cbd8273
Pepe Doval <pepellou@gmail.com>
2012-02-15 11:15:34 +0100

    Method for dynamic loading of Problems Solvers

--- src/katas/refactor/euler/Euler.java
+++ src/katas/refactor/euler/Euler.java
@@ -2,16 +2,31 @@ package katas.refactor.euler;
 
 public class Euler {
 
+	private String solveProblem(int numProblem) {
+		try {
+			return ((Problem) Class.forName(
+					"katas.refactor.euler.Problem" + numProblem).newInstance())
+					.solve();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	public String problem1() {
-		return new Problem1().solve();
+		return solveProblem(1);
 	}
 
 	public String problem2() {
-		return new Problem2().solve();
+		return solveProblem(2);
 	}
 
 	public String problem10() {
-		return new Problem10().solve();
+		return solveProblem(10);
 	}
 
 }
